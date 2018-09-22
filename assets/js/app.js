2c81af595d5feb34989cbfd9d933ac7c0f78e1e6
Roberto aguero <rjaguero00@gmail.com>
2018-09-22 11:16:09 -0700

    change location/local storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,35 @@
+//Init localStorage
+const storage = new Storage();
+//Get stored location data
+const weatherLocation = storage.getLocationData();
 //Init wheather object
-const weather = new Weather('Boston', 'MA');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 //Init UI
 const ui = new UI();
+
 
 // weather.changeLocation('Miami', 'FL');
 
 //Get weather on DOM load 
 document.addEventListener('DOMContentLoaded', getWeather);
+
+//Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    //Change location
+    weather.changeLocation(city, state);
+
+    //Set location in Local Storage
+    storage.setLocationData(city, state);
+
+    //Get and display weather
+    getWeather();
+
+    //Close Modal
+    $('#locModal').modal('hide');
+});
 
 function getWeather() {
     weather.getWeather()
