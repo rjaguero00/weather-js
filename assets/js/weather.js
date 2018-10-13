c95b8ae88605ed8e14b2fd37b0b8dbd13927a464
Roberto aguero <rjaguero00@gmail.com>
2018-10-13 12:24:14 -0500

    api fix

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -6,7 +6,7 @@ class Weather {
     }
 
     async getWeather() {
-        const response = await fetch(`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
+        const response = await fetch(`https://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
 
         const responseData = await response.json();
 
